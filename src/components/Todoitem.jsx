dab52d6d5dada0320cb73e4613c733c31b915438
Dustin De Jager <dustindejager84@gmail.com>
2025-03-31 17:26:07 +0200

    some styling updates to list items

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -5,18 +5,20 @@ export function Todoitem({completed, title, id, toggleTodo, deleteTodo}) {
    
   return (
     <ListItem id={id}>
-      <div htmlFor="listItem">
-      <Checkbox value={completed} onChange={e => toggleTodo(id, e.target.checked)}  />
-          {title}
-
-          <Button onClick={() => deleteTodo(id)} 
-            color="secondary" 
-            variant="contained" 
-            className='btn btn-danger' edge="end">
-              Delete
-          </Button>
+      <div>
+        <input type="checkbox" value={completed} onChange={e => toggleTodo(id, e.target.checked)}  />
+        <div className="item">
+          
+        {/* <Checkbox value={completed} onChange={e => toggleTodo(id, e.target.checked)}  /> */}
+          {title}</div>
+        <Button onClick={() => deleteTodo(id)} 
+          color="secondary" 
+          variant="contained" 
+          className='btn btn-danger' edge="end">
+            Delete
+        </Button>
       </div>
-
+      <div className="clearfix"></div>
     </ListItem>
   )
 }
